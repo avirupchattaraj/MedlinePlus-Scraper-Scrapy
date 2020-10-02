dc0fcce88f6ed8716d0e49127eb2119606dd0873
Avirup Chattaraj <avi14chattarajyu@gmail.com>
2020-10-02 20:14:18 +0530

    scraped the links next on to drugs

--- medlineplus/spiders/drugs.py
+++ medlineplus/spiders/drugs.py
@@ -4,7 +4,12 @@ import scrapy
 class DrugsSpider(scrapy.Spider):
     name = 'drugs'
     allowed_domains = ['medlineplus.gov/druginformation.html']
-    start_urls = ['http://medlineplus.gov/druginformation.html/']
+    start_urls = ['https://medlineplus.gov/druginformation.html']
 
     def parse(self, response):
-        pass
+        browse = response.xpath(".//ul[@class='alpha-links']//li")
+        for link in browse:
+            value = link.xpath("./a/@href").get()
+            yield {
+                'value': value
+            }
