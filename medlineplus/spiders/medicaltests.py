ecf93209516e6cd6e89e4e590c27863b95086b1a
Avirup Chattaraj <avi14chattarajyu@gmail.com>
2020-10-04 23:15:52 +0530

    initial spider configuration

--- medlineplus/spiders/medicaltests.py
+++ medlineplus/spiders/medicaltests.py
@@ -3,8 +3,8 @@ import scrapy
 
 class MedicaltestsSpider(scrapy.Spider):
     name = 'medicaltests'
-    allowed_domains = ['medlineplus.gov/lab-tests']
-    start_urls = ['http://medlineplus.gov/lab-tests/']
+    allowed_domains = ['medlineplus.gov']
+    start_urls = ['https://medlineplus.gov/lab-tests/']
 
     def parse(self, response):
         pass
